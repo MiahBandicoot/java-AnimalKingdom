afff45eb32210dd6214bbb07386c0d6807d4f46f
Miah <revolution5117@gmail.com>
2020-10-28 06:32:51 -0700

    Working on atom text editor.

--- src/main/bird.java
+++ src/main/bird.java
@@ -2,9 +2,33 @@ package main;
 
 public abstract class Bird extends Animal{
   Super();
+  private String type;
   private String fly;
   private String lungs;
   private String eggs;
+
+
+  public Bird(String type, String fly, String lungs, String eggs){
+    this.type = type,
+    this.fly = fly,
+    this.lungs, = lungs,
+    this.eggs = eggs,
+  }
+  public getType(){
+    return type;
+  }
+  public void setType(String type){
+    this.type = type;
+  }
+  public void setWalk(String fly){
+    System.out.println("Flap Flap")
+  };
+  public void setLungs(String lungs){
+    this.lungs = lungs;
+  };
+  public void setLiveBirths(String eggs){
+    this.eggs = eggs;
+  };
   void fly();
   void lungs();
   void eggs();
--- src/main/mammal.java
+++ src/main/mammal.java
@@ -3,10 +3,30 @@ package main;
 
 public abstract class Mammal extends Animal{
   Super();
+  private String type;
   private String walk;
   private String lungs;
   private String liveBirths;
-  void walk();
-  void lungs();
-  void liveBirths();
+
+  public Mammal(String type, String walk, String lungs, String liveBirths){
+    this.type = type,
+    this.walk = walk,
+    this.lungs, = lungs,
+    this.liveBirths = liveBirths,
+  }
+  public getType(){
+    return type;
+  }
+  public void setType(String type){
+    this.type = type;
+  }
+  public void setWalk(String walk){
+    System.out.println("Walk")
+  };
+  public void setLungs(String lungs){
+    this.lungs = lungs;
+  };
+  public void setLiveBirths(String liveBirths){
+    this.liveBirths = liveBirths;
+  };
 }
